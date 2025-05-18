f66f11694c99eb0d3887981e4a3872d50453df07
amaniCoding <amanuelfrm@gmail.com>
2025-05-18 15:45:56 +0300

    Update next.config.ts
    configured nextconfig file

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+/** @type {import('next').NextConfig} */
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -5,14 +6,14 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "my-blob-store.public.blob.vercel-storage.com",
+        hostname: "7gqxz60aa5shpy47.public.blob.vercel-storage.com",
         port: "",
       },
     ],
   },
 };
 
-/** @type {import('next').NextConfig} */
+
 
 module.exports = {
   experimental: {
