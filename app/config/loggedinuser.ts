91443f9970b8a155a9b62545f37f8caf865b7c0e
amanyCoding <amanuelfrm@gmail.com>
2025-06-08 16:34:25 +0300

    updated some features

--- app/config/loggedinuser.ts
+++ app/config/loggedinuser.ts
@@ -4,5 +4,5 @@ export const LoggedInUser: User = {
   userid: "3430f909-7972-4b4d-be93-9fc6a13ca911",
   fname: "Amanuel",
   lname: "Ferede",
-  profilepic: "/feeds/4.jpg",
+  profilepic: "/feeds/1.jpg",
 };
