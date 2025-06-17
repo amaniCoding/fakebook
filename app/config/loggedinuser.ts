b5e6d72e59c3f70c5f31a0380bf9ad3b125a4adb
amanyCoding <amanuelfrm@gmail.com>
2025-06-17 19:15:32 +0300

    added redux features

--- app/config/loggedinuser.ts
+++ app/config/loggedinuser.ts
@@ -1,4 +1,4 @@
-import { User } from "../libs/data/user/types";
+import { User } from "../types/db/query/user";
 
 export const LoggedInUser: User = {
   userid: "3430f909-7972-4b4d-be93-9fc6a13ca911",
