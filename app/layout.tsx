7ea2cfa3959bac36e23c7c57e38415e316153641
amanyCoding <amanuelfrm@gmail.com>
2025-06-21 16:04:03 +0300

    this hobby

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <div className="h-screen">
+        <div className="h-screen overflow-y-auto">
           <AppProvider>{children}</AppProvider>
         </div>
       </body>
