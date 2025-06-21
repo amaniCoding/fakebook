c0beb74f4abd2a2d7ad627abed5be9f53b4c7f52
amanyCoding <amanuelfrm@gmail.com>
2025-06-21 15:55:17 +0300

    this hobby

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <div className="h-screen">
+        <div className="min-h-screen">
           <AppProvider>{children}</AppProvider>
         </div>
       </body>
